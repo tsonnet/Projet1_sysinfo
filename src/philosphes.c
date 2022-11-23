3761d233ee0545e7498b89ac6cd4cc21caa8e407
tsonnet <thibaut.sonnet@uclouvain.be>
2022-11-23 10:24:20 +0100

    good

--- src/philosphes.c
+++ src/philosphes.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 
-int main(int argc, char* argv[]){
+#include <stdlib.h>
 
-    int N = (int) argv[1];
-    printf("%d   \n", N);
+int main(int argc, char* argv[]){
+    int N = atoi(argv[1]);
     
     return 0;
+}
+
+int philosphoes(int N_phil, int cycle){
+
 
 }
