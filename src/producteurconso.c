2bd3d2b99b501e6cf5130c90e46d4ebd1a686623
Diaby02 <nicolas.bourez@student.uclouvain.be>
2022-11-23 10:24:40 +0100

    change producers

---
+++ src/producteurconso.c
@@ -0,0 +1,18 @@
+int number_of_consumers;
+int numbers_of_threads;
+
+void consumers(){
+
+}
+
+void producers(){
+    
+}
+
+int main(int argc, char* argv[]){
+
+    number_of_consumers = atoi(argv[0]);
+    numbers_of_threads = atoi(argv[1]);
+
+    return 0;
+}
