584d989563f2fb658d9ea6c9ced88e56f539754b
tsonnet <thibaut.sonnet@uclouvain.be>
2022-11-30 00:01:25 +0100

    t

--- src/philosphes.c
+++ src/philosphes.c
@@ -1,14 +1,69 @@
-#include<stdio.h>
-
+#include <pthread.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
 
-int main(int argc, char* argv[]){
-    int N = atoi(argv[1]);
-    
-    return 0;
+
+int PHILOSOPHES=0;
+pthread_t* phil;
+pthread_mutex_t* baguette;
+
+
+void mange(int id) {
+    printf("Philosophe [%d] mange\n",id);
+    for(int i=0;i< rand(); i++) {
+    // philosophe mange
+    }
+}
+void* philosophe ( void* arg )
+{
+    int id = *((int*)arg);
+    int n=0;
+    int left = id;
+    int right = (left + 1) % PHILOSOPHES;
+    while(true) {
+        // philosophe pense
+        if(left<right) {
+            pthread_mutex_lock(&baguette[left]);
+            pthread_mutex_lock(&baguette[right]);
+        }
+        else {
+            pthread_mutex_lock(&baguette[right]);
+            pthread_mutex_lock(&baguette[left]);
+        }
+        //mange(id);
+        pthread_mutex_unlock(&baguette[left]);
+        pthread_mutex_unlock(&baguette[right]);
+        n++;
+        if (n >= 100000) break; 
+        printf("Nombre de Philosphes %d left %d right %d\n", PHILOSOPHES,left,right);
+        fflush(stdout);
+    }
+    return (NULL);
 }
 
-int philosphoes(int N_phil, int cycle){
 
+int main(int argc, char* argv[]){
+    if (argc > 1){
+        PHILOSOPHES = atoi(argv[1]);
+    }
+    int* id = malloc(sizeof(int)*PHILOSOPHES);
+    phil = malloc(PHILOSOPHES*sizeof(pthread_t));
+    baguette = malloc(sizeof(pthread_mutex_t)*PHILOSOPHES);
+    for( int i=0 ; i< PHILOSOPHES;i++){
+        id[i]=i;
+        pthread_mutex_init(baguette+i,NULL);
+    }
+    for(int i =0; i< PHILOSOPHES;i++){
+        pthread_create(phil+i, NULL ,philosophe,(void *) id+i);
+    }
+    for(int i =0; i< PHILOSOPHES;i++){
+        pthread_join(phil[i],NULL);
+    }
 
+    free(phil);
+    free(baguette);
+    free(id);
+    return 0;
 }
