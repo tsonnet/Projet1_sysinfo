a4384c36680cd7ec19eda37a6e5aa210d6d16f60
tsonnet <thibaut.sonnet@uclouvain.be>
2022-11-30 10:10:27 +0100

    ok

--- Timer/correction.sh
+++ Timer/correction.sh
@@ -5,6 +5,6 @@ THREADS=(2 4 8 ) # ON met directement deux philo car bug avec 1
 echo "thread,i,time"
 for thread in "${THREADS[@]}"; do
 	for i in {1..5}; do
-		/usr/bin/time -f "$thread,$i,%E" ./philosophes  $thread 
+		/usr/bin/time -f "$thread,$i,%E" ./philosophes  $thread 2>&1
 	done
 done
