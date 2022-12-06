898e89932e7202a04c56cf86bc4ebe450f4c77e1
tsonnet <thibaut.sonnet@uclouvain.be>
2022-12-06 11:12:52 +0100

    te

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ INCLUDE_HEADERS_DIRECTORY=-Iheaders
 
 # ligne 9 :this will run the following command: gcc -Wall -Werror -g -o kmeans src/distance.o other_object_filespresent_above.o ... -lcunit -lpthread
 
-all : all_exe all_data python #zip
+all : all_exe all_data python python_inginious#zip
 
 all_exe : philosophes prodcons lececriv  #compile les 3 fonctions
 
@@ -13,7 +13,11 @@ all_exe2 : philosophes2 prodcons2 lececriv2
 
 all_data: data_philo data_lececriv data_prodcons #compile tous les csv
 
+<<<<<<< HEAD
 all_data2 : data_philo2 data_prodcons2
+=======
+all_data2: data_philo2 data_lececriv2 data_prodcons2 #compile tous les csv
+>>>>>>> f6115f20a022826897f8282bb76b6e38173438d3
 
 clean_all : clean_exe clean_csv clean_python clean_zip
 
@@ -49,28 +53,50 @@ data_prodcons: #compile le csv de prodcons
 
 data_prodcons2: #compile le csv de prodcons
 	cd Part2/Timer;\
+<<<<<<< HEAD
 	./prodconsOurSem.sh > prodcons.csv;\
 	cd ../..;
 
 lececriv: Part1/src/lececriv.c  # compile lececriv # add your other object files needed to compile your program here. !! The ordering is important !! if file_a.o depends on file_b.o, file_a.o must be placed BEFORE file_b.o in the list !
+=======
+	./ProdconsOurSem.sh > prodcons2.csv;\
+	cd ../..;
+
+lececriv: Part1/src/lececrivOurSem.c  # compile lececriv # add your other object files needed to compile your program here. !! The ordering is important !! if file_a.o depends on file_b.o, file_a.o must be placed BEFORE file_b.o in the list !
+>>>>>>> f6115f20a022826897f8282bb76b6e38173438d3
 	$(CC) $(INCLUDE_HEADERS_DIRECTORY) $(CFLAGS) -o $@ $^ $(LIBS) 
 	mv $@ Part1/src/
+
+lececriv2: Part2/src/lececrivOurSem.c  Part2/src/Semimplem.c # compile lececriv # add your other object files needed to compile your program here. !! The ordering is important !! if file_a.o depends on file_b.o, file_a.o must be placed BEFORE file_b.o in the list !
+	$(CC) $(INCLUDE_HEADERS_DIRECTORY) $(CFLAGS) -o $@ $^ $(LIBS) 
+	mv $@ Part2/src/
 
 data_lececriv: #compile le csv de lececriv
 	cd Part1/Timer;\
 	./lececriv.sh > lececriv.csv;\
 	cd ../..;
 
+data_lececriv2: #compile le csv de lececriv
+	cd Part2/Timer;\
+	./LececrivOurSem.sh > lececriv2.csv;\
+	cd ../..;
+
 %.o: %.c                  # if for example you want to compute example.c this will create an object file called example.o in the same directory as example.c. Don't forget to clean it in your "make clean"
 	$(CC) $(INCLUDE_HEADERS_DIRECTORY) $(CFLAGS) -o $@ -c $<
 
 python: 
-	python3 Part1/Timer/script_python.py continue plot
-	python3 Part1/Timer/script_python.py boxplot plot
-	python3 Part1/Timer/script_python.py continue subplot
-	python3 Part1/Timer/script_python.py boxplot subplot
+	python3 Part1/Timer/script_python.py continue plot Part1/Timer/philosophes.csv Part1/Timer/lececriv.csv Part1/Timer/prodcons.csv ordi
+	python3 Part1/Timer/script_python.py boxplot plot Part1/Timer/philosophes.csv Part1/Timer/lececriv.csv Part1/Timer/prodcons.csv ordi
+	python3 Part1/Timer/script_python.py continue subplot Part1/Timer/philosophes.csv Part1/Timer/lececriv.csv Part1/Timer/prodcons.csv ordi
+	python3 Part1/Timer/script_python.py boxplot subplot Part1/Timer/philosophes.csv Part1/Timer/lececriv.csv Part1/Timer/prodcons.csv ordi
 	mv *.png Part1/Plots/
 
+python_inginious:
+	python3 Part1/Timer/script_python.py continue plot Part1/Timer/ResultPArt1/philosophes.csv Part1/Timer/ResultPArt1/lececriv.csv Part1/Timer/ResultPArt1/prodcons.csv inginious
+	python3 Part1/Timer/script_python.py boxplot plot Part1/Timer/ResultPArt1/philosophes.csv Part1/Timer/ResultPArt1/lececriv.csv Part1/Timer/ResultPArt1/prodcons.csv inginious
+	python3 Part1/Timer/script_python.py continue subplot Part1/Timer/ResultPArt1/philosophes.csv Part1/Timer/ResultPArt1/lececriv.csv Part1/Timer/ResultPArt1/prodcons.csv inginious
+	python3 Part1/Timer/script_python.py boxplot subplot Part1/Timer/ResultPArt1/philosophes.csv Part1/Timer/ResultPArt1/lececriv.csv Part1/Timer/ResultPArt1/prodcons.csv inginious
+	mv *.png Part1/Plots/
 zip:
 	zip -r target2.zip Part1 Part2 Makefile README.md Experiments experiments.sh
 
---
+++ Part2/src/lececrivOurSem.c
@@ -0,0 +1,195 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <semaphore.h>
+#include "../Headers/OurSem.h"
+
+
+int mutex_readcount;
+int mutex_writecount;
+int mutex;
+
+struct Our_semInit* wsem;  // accès à la db
+struct Our_semInit* rsem;
+
+int readcount=0; // nombre de readers
+int writecount=0;
+
+int total_writings = 0;
+int total_readings = 0;
+int readings = 1;
+int writings = 1;
+
+
+void processing_CPU(void){
+    int count = 0;
+    for (int i = 0; i < 10000; i++)
+    {
+        count++;
+    }    
+}
+
+void* writer(void* id){
+    //int id_int = (intptr_t) id;
+
+    while(total_writings < writings){
+            ////printf("le writer %d accède à sa fonction et est bloqué !!\n",id_int);
+            lock(&mutex_writecount);
+                  ////printf("le writer %d est débloqué, ouf !\n",id_int);
+                  writecount++;
+                  if(writecount == 1){
+                      ////printf("le writer %d attend un reader et debloque le mutex\n",id_int);
+                      OurSemWait(rsem); //attend plus de readers
+
+                  }
+            unlock(&mutex_writecount);
+            
+            //printf("le writer %d attend un signal\n",id_int);
+            OurSemWait(wsem);
+              //printf("le writer %d a reçu un signal et lance sa fonction\n",id_int);
+              // section critique, un seul writer à la fois
+              if(total_writings != writings){
+                processing_CPU();
+                total_writings++;
+              }
+            OurSemPost(wsem);
+            //printf("le writer %d envoie un signal wsem et est bloqué\n",id_int);
+
+            lock(&mutex_writecount);
+                  //printf("le writer %d est débloqué, deuxième ouf !\n",id_int);
+                  writecount--;
+                  if (writecount == 0){
+                      //printf("le writer %d envoie un signal rsem et debloque le mutex\n",id_int);
+                      OurSemPost(rsem); //autorise les readers
+                  }
+            unlock(&mutex_writecount);
+            //printf("le writer %d finit sa boucle\n",id_int);
+                      
+
+    }
+    //printf("le writer %d sort de sa boucle\n",id_int);
+            
+    return (NULL);
+}
+
+
+
+void* reader(void* id){
+    //int id_int = (intptr_t) id;
+
+    while(total_readings < readings){
+            //printf("le reader %d accède à sa fonction et est bloqué !!\n",id_int);
+            
+            lock(&mutex); //un seul reader à la fois a acces à sem_wait
+            //printf("le reader %d est débloqué et attend un signal rsem\n",id_int);
+                  
+            OurSemWait(rsem); //n'avoir qu'un seul reader qui lit
+            //printf("le reader %d est bloqué et est a reçu son signal rsem\n",id_int);
+                  
+            lock(&mutex_readcount);
+              // section critique
+              readcount++;
+              if (readcount==1)
+              { // arrivée du premier reader
+                //printf("le reader %d est solo et attend wsem\n",id_int);
+        
+                OurSemWait(wsem);// je suis entrain de lire, vous ne pouvez plus écrire
+              }
+              if(writecount == 1){
+                  //printf("le reader %d est bloqué envoie un signal rsem\n",id_int);
+                  OurSemPost(wsem);
+              }
+            unlock(&mutex_readcount);
+            OurSemPost(rsem);
+
+            unlock(&mutex);
+
+            if(total_readings != readings){
+                total_readings++;
+                processing_CPU();
+            }
+
+            lock(&mutex_readcount);
+              // section critique
+              readcount--;
+              if(readcount==0)
+              { // départ du dernier reader
+                OurSemPost(wsem);
+              }
+            unlock(&mutex_readcount);
+    }
+    return (NULL);
+}
+
+int main(int argc, char* argv[]){
+    //double time; 
+    //clock_t t1,t2;
+    //t1 = clock();
+
+    
+    if(argc != 5){
+        //printf("Not enough arguments");
+        return 0;
+    }
+    
+    int nb_readers = atoi(argv[1]);
+    int nb_writers = atoi(argv[2]);
+    readings = atoi(argv[3]);
+    writings = atoi(argv[4]);
+
+    wsem = InitOurSem(1);
+    rsem = InitOurSem(1);
+
+    pthread_t Readers_threads[nb_readers];
+    pthread_t Writers_threads[nb_writers];
+
+    mutex_writecount = 0;
+    mutex_readcount = 0;
+    mutex = 0;
+
+        // Create n Readers
+    for (int i = 0; i < nb_writers; i++) {
+        
+        pthread_create(&Readers_threads[i], NULL, reader, (void *) (intptr_t) i);
+        //printf("Created Reader Thread = %ld count = %d \n", Readers_threads[i],i);
+    }
+
+    // Create n Write
+    for (int i = 0; i < nb_readers; i++) {
+        
+        pthread_create(&Writers_threads[i], NULL, writer, (void *) (intptr_t) i);
+        //printf("Created Wrtier Thread = %ld count = %d \n", Writers_threads[i],i);
+    }
+
+        // join all of the producer threads
+    for (int i = 0; i < nb_writers; i++) {
+        
+        pthread_join(Readers_threads[i], NULL);
+        nb_writers--;
+        //printf("join Reader thread = %d \n", i);
+    }
+
+    // join all of the consumer threads
+    for (int i = 0; i < nb_readers; i++) {
+        
+        pthread_join(Writers_threads[i], NULL);
+        nb_readers--;
+        //printf("join Writer thread = %d \n", i);
+    }
+
+
+    OurSemDestroy(wsem);
+    OurSemDestroy(rsem);
+
+    //printf("Total readings = %d and Total writings = %d\n", total_readings,total_writings);
+    //t2 = clock() - t1;
+    //time = ((double)t2)/CLOCKS_PER_SEC;
+    //printf("\nTemps de conversion :%.6f\n",time);
+    return 0;
+}
